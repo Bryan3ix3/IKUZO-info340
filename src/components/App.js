227dd927268d3c432360d67b6e2008f1fd3df309
jwu23 <jwu23@uw.edu>
2021-11-22 11:22:03 -0800

    added Routes and create Home component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,23 @@
 import React, { useState } from 'react';
 import _ from 'lodash';
 import { NavBar } from './Navigation';
-import { Filter } from './Filter';
+import { HomeScreen } from './Home';
 import { Footer } from './Footer';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
-    <body className="general">
-      <NavBar />
-      <main>
-        <Filter />
-      </main>
-      <Footer />
-    </body>
+    <BrowserRouter>
+      <body className="general">
+        <NavBar />
+        <main>
+          <Routes>
+            <Route exact path="/" element={<HomeScreen />}></Route>
+          </Routes>
+        </main>
+        <Footer />
+      </body>
+    </BrowserRouter>
   );
 }
 
