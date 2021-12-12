3137a9a4f9bd98e265c8d2b6006dfa3479902d86
William Zhang <wzhang26@uw.edu>
2021-12-11 17:39:44 -0800

    added modals for mobile view

--- src/components/FriendButton.js
+++
@@ -1,8 +0,0 @@
-
-export function FriendsButton(props) {
-    return(
-      <button id="friend-icon" onClick={props.handleModalCallback()}>
-          <img src={"img/friend.png"} alt="Friend icon" />
-      </button>
-    );
-  }
